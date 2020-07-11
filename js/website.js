2e277c7e5e53be5c68126cf4a876c5516962a3c8
deveshprasad <63739986+deveshprasad@users.noreply.github.com>
2020-07-11 21:04:00 +0530

    Add files via upload

---
+++ js/website.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function(){
+    "use strict"
+    $('.home').ripples({
+        dropRadius:15,
+        perturbance:0.01,
+    });
+    var typed = new Typed('.text', {
+        strings: ["<strong>Happy</strong><strong class='primary'> coding</strong>😁", "<strong>happy</strong><strong class='primary'> analysing</strong>  😇 "],
+        typeSpeed: 100,
+        loop:true
+        
+    });
+    
+  
+      
+
+    
+});
+
+
